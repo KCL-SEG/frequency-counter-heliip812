ad1cd95c3a92e5f52d026e99e468ad64a4c6d274
heliip812 <heliip812@gmail.com>
2022-12-05 16:14:50 +0000

    attempt

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for x in items:
+        if str(x) in frequencies:
+            frequencies[str(x)] = frequencies[str(x)]+1
+        else:
+            frequencies[str(x)]=1
     return frequencies
